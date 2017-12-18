297a465db493bea9272ea8067302bba5036d7576
Jonas Herdeg <jonas.herdeg@gmail.com>
2017-12-18 18:39:00 +0100

    Try to fix

--- angular-src/src/app/components/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -89,7 +89,7 @@ export class DashboardComponent implements OnInit {
 
 	//Hier werden Nachrichten die in der Datenbank gespeichert sind ausgegeben
 
-	this.socketService.output($chat, msgTo);
+	//this.socketService.output($chat, msgTo);
 	this.socketService.load(msgTo);
 
 
