075f8fd76957813f801782b45e3534f62f6360ff
Jonas Herdeg <jonas.herdeg@gmail.com>
2017-12-18 08:21:50 +0100

    Upload

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+	database: 'mongodb://jonas:1234@ds123146.mlab.com:23146/meanauthapp',
+	secret: 'yoursecret'
+}
---
+++ models/chats.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const config = require('../config/database');
+
+// User Schema
+const ChatSchema = mongoose.Schema({
+	message : {
+		type : String
+	},
+	from : {
+		type : String
+	},
+	to : {
+		type : String
+	}
+});
+
+const Chats = module.exports = mongoose.model('Chats', ChatSchema);
+
+module.exports.getChatById = function(id, callback) {
+	Chats.findById(id, callback);
+};
+
+module.exports.getChatsBySender = function(sender, callback) {
+	const query = {from: sender}
+	Chats.findOne(query, callback);
+};
